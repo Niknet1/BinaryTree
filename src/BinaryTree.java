66b5a7de4830e16fddd3af2b210b2634ac2747fb
Niknet1 <89482298+Niknet1@users.noreply.github.com>
2023-02-10 07:34:12 -0600

    Update BinaryTree.java

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -51,45 +51,96 @@ public class BinaryTree {
         if (root == null) return null;
         BinaryNode temp = root;
         BinaryNode inorderSuccessor;
-        if (root.getLeft() == null && root.getRight() == null) {
-            root = null;
-            return temp;
-        }
-//remove root degree 1 – right child
-        else if (root.getLeft() == null) {
-            root = root.getRight();
-            temp.setRight(null);
-            ;
-            return temp;
-        }
-//remove root degree 1 – left child
-        else if (root.getRight() == null) {
-            root = root.getLeft();
-            temp.setLeft(null);
-            return temp;
-        }
-//remove root degree 2
-        else {
-            inorderSuccessor = successor(root);
-            swap(root, inorderSuccessor);
-            if (root.getRight() == inorderSuccessor) {
-                root.setRight(inorderSuccessor.getRight());
-                inorderSuccessor.setRight(null);
-                return inorderSuccessor;
+        if(root.getValue().equals(target)) {
+            if (root.getLeft() == null && root.getRight() == null) {
+                root = null;
+                return temp;
             }
-            return remove(root.getRight(), target);
+//remove root degree 1 – right child
+            else if (root.getLeft() == null) {
+                root = root.getRight();
+                temp.setRight(null);
+                ;
+                return temp;
+            }
+//remove root degree 1 – left child
+            else if (root.getRight() == null) {
+                root = root.getLeft();
+                temp.setLeft(null);
+                return temp;
+            }
+//remove root degree 2
+            else {
+                inorderSuccessor = successor(root);
+                swap(root, inorderSuccessor);
+                if (root.getRight() == inorderSuccessor) {
+                    root.setRight(inorderSuccessor.getRight());
+                    inorderSuccessor.setRight(null);
+                    return inorderSuccessor;
+                }
+                return remove(root.getRight(), target);
+            }
         }
         //if root is not removed call remove helper method
-
         return remove(root, target);
 
 
     }
 
 
-    private BinaryNode remove(BinaryNode k, Comparable x);{
 
-    }
+        private BinaryNode remove(BinaryNode startNode, Comparable target)
+        {
+            BinaryNode nodeToRemove, inorderSuccessor;
+            BinaryNode parent = search(startNode,target);
+            if(parent == null) return null;
+            //decide if it is a left or right child
+            boolean isLeft = parent.getLeft()!=null && parent.getLeft().getValue().equals(target);
+            nodeToRemove = isLeft ? parent.getLeft() : parent.getRight();
+            //degree 0
+            if(nodeToRemove.getLeft() == null && nodeToRemove.getRight() == null)
+            {
+                if(isLeft)
+                    parent.setLeft(null);
+                else
+                    parent.setRight(null);
+                return nodeToRemove;
+            }
+            //degree 1
+            else if(nodeToRemove.getLeft() == null)
+            {
+                if(isLeft)
+                    parent.setLeft(nodeToRemove.getRight());
+                else
+                    parent.setRight(nodeToRemove.getRight());
+                nodeToRemove.setRight(null);
+                return nodeToRemove;
+            }
+            else if(nodeToRemove.getRight() == null)
+            {
+                if(isLeft)
+                    parent.setLeft(nodeToRemove.getLeft());
+                else
+                    parent.setRight(nodeToRemove.getLeft());
+                nodeToRemove.setLeft(null);
+                return nodeToRemove;
+            }
+            //degree 2
+            else
+            {
+                inorderSuccessor = successor(nodeToRemove);
+                swap(inorderSuccessor, nodeToRemove);
+                if(nodeToRemove.getRight()==inorderSuccessor)
+                {
+                    nodeToRemove.setRight(inorderSuccessor.getLeft());
+                    inorderSuccessor.setRight(null);
+                    return inorderSuccessor;
+                }
+                return remove(nodeToRemove.getRight(), target);
+            }
+
+        }
+
 
 
     private void swap(BinaryNode k, BinaryNode i) {
